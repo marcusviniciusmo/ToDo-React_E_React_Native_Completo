6dc56e2967c118b4d6b620b6aa95440831a7b263
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-07-25 10:21:14 -0300

    Aula 31 - Exibindo as tarefas atrasadas

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -1,5 +1,6 @@
 const TaskModel = require('../model/TaskModel');
-const { response } = require('express');
+
+const current = new Date();
 
 class TaskController {
     
@@ -64,6 +65,21 @@ class TaskController {
             {'_id': req.params.id},
             {'done': req.params.done},
             {new: true})
+            .then(response => {
+                return res.status(200).json(response);
+            })
+            .catch(error => {
+                return res.status(500).json(error);
+            });
+    }
+
+    async late(req, res){
+        await TaskModel
+            .find({
+                'when': {'$lt': current},
+                'macaddress': {'$in': req.body.macaddress}
+            })
+            .sort('when')
             .then(response => {
                 return res.status(200).json(response);
             })
--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -11,5 +11,6 @@ router.get('/:id', TaskController.show);
 router.delete('/:id', TaskController.delete);
 router.put('/:id/:done', TaskController.done);
 router.get('/filter/all', MacaddressValidation ,TaskController.all);
+router.get('/filter/late', MacaddressValidation, TaskController.late);
 
 module.exports = router;
