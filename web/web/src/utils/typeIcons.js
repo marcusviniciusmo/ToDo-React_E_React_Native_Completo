e5352c11a467a12afb69c582487b7379a3266737
Marcus Vinícius <marcusoliveira@pmenos.com.br>
2020-07-29 09:18:20 -0300

    Aula 47 - Deixando os ícones das Tarefas Dinâmicos

---
+++ web/web/src/utils/typeIcons.js
@@ -0,0 +1,25 @@
+// ICONES
+import iconDefault from '../assets_web/default.png';
+import iconFootball from '../assets_web/football.png';
+import iconFood from '../assets_web/food.png';
+import iconJob from '../assets_web/job.png';
+import iconParty from '../assets_web/party.png';
+import iconStudy from '../assets_web/study.png';
+import iconShopping from '../assets_web/shopping.png';
+import iconTravel from '../assets_web/travel.png';
+import iconSport from '../assets_web/sport.png';
+
+const typeIcons = [
+    null,
+    iconDefault,
+    iconFootball,
+    iconFood,
+    iconJob,
+    iconParty,
+    iconStudy,
+    iconShopping,
+    iconTravel,
+    iconSport
+];
+
+export default typeIcons;
