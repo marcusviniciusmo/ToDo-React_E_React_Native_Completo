cfa77e911c13260169091ed7412af9a9def2c9e0
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-08-27 05:27:35 -0300

    Aula 68 - Criação dos Filtros

--- todo/App.js
+++ todo/App.js
@@ -2,6 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Home from './src/views/Home';
 
+// Desabilita os alertas
+console.disableYellowBox = true;
+
 export default function App() {
   return <Home />
 }
